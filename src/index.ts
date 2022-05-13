1f58f45649cee1300124b3b9693f8c5d1acd769b
HMasataka <siruba.2050@gmail.com>
2022-05-13 17:39:09 +0900

    Feat db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express'
+import mysql, { createConnection } from 'mysql';
 
 const app: express.Application = express()
-const PORT = 3000
+const PORT: number = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -13,7 +14,23 @@ app.listen(PORT, () => {
 })
 
 async function helloWorld(_: express.Request, res: express.Response) {
-  res.status(200).send({
-    message: 'Hello,World!',
+  const connection = conn()
+  connection.query("SELECT * FROM users;",(err, rows, _) => {
+    if (err) throw err;
+    res.status(200).send({message:rows})
+  });
+  connection.end();
+}
+
+function conn(): mysql.Connection {
+  const connection = createConnection({
+    host: "localhost",
+    port: 33060,
+    database: "db",
+    user: "user",
+    password: "password",
+    multipleStatements: true,
   })
+
+  return connection
 }
