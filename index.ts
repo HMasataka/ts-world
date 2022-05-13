b1b3da41013154e6870d1fc5e47adfc7ad000c84
HMasataka <siruba.2050@gmail.com>
2022-05-13 16:20:37 +0900

    Refactor index.ts

--- index.ts
+++ index.ts
@@ -1,23 +1,17 @@
-import express, { Application, Request, Response } from 'express'
+import express from 'express'
 
-const app: Application = express()
+const app: express.Application = express()
 const PORT = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', async (_req: Request, res: Response) => {
+app.get('/', async (req: express.Request, res: express.Response) => {
   return res.status(200).send({
     message: 'Hello World!',
   })
 })
 
-try {
-  app.listen(PORT, () => {
-    console.log(`dev server running at: http://localhost:${PORT}/`)
-  })
-} catch (e) {
-  if (e instanceof Error) {
-    console.error(e.message)
-  }
-}
+app.listen(PORT, () => {
+  console.log(`dev server running at: http://localhost:${PORT}/`)
+})
